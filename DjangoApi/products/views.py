dbe9a48967f59d907f2f3d5e295e2f5d82e5b5f6
leestana01 <leestana01@naver.com>
2023-05-12 21:13:00 +0900

    백엔드 온보드 Chapter 13,14 완료

---
+++ DjangoApi/products/views.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .serializer import ProductSerializer
+from .models import Product
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
