4bd97d401474cfed7c9aff941a32d08a739349ee
leestana01 <leestana01@naver.com>
2023-05-17 00:25:32 +0900

    Chapter 3 완료

---
+++ lotto2/demos/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+def lotto_index(request):
+    return render(request, 'lotto_index.html')
+
+def lotto_result(request):
+    lotto_number = list()
+    game = request.GET.get('game',1)
+    pull_number = [index for index in range(1,46)]
+
+    for _ in range(int(game)):
+        lotto_number.append(random.sample(pull_number,6))
+
+    return render(request, 'lotto_result.html', {'lotto_number' : lotto_number, 'game': game})
